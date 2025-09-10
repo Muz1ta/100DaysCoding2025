c755b6d91344ef0f4e1359638f4a2fd2988aa33a
Muz1ta <muzitaainnun@gmail.com>
2025-09-10 16:36:46 +0800

    Day2.java

--- Day2.java
+++ Day2.java
@@ -2,9 +2,9 @@ package CODINGISC;
 
 public class miaw {
     public static void main(String[] args){
-    System.out.print("Haii! i'am Muzita " );
+    System.out.print ("Haii! i'am Muzita)" ;
         System.out.println("Mochi Lovers");
-        String age =("I'm 18 years old");
+        String age = "I'm 18 years old";
         System.out.printf("%s", age);
     }
 }
