d6e98288090c2f910e44d2d906be15ff86832159
Muz1ta <muzitaainnun@gmail.com>
2025-09-10 16:33:53 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,14 @@
+public class Day3 {
+    public static void main (String [] args) {
+        // menampilkan tulisan " hai ! " 
+        System.out.println("Hello!");
+        /** masukkan variabel setelah tipe data
+         *  masukkan angka setelah samadengan sesuai tipe data
+         */ 
+       String M = "Strawberry ";
+        double Lahir = 73.2007;
+        // menampilkan Strawberry dan  M=73.2007
+        System.out.printf(M, "%s/n " );
+        System.out.printf("Lahir = %.4f", Lahir );
+    }
+}
