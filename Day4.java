fc4002e31da18a9d36f95de4ea8db6e775e74714
Muz1ta <muzitaainnun@gmail.com>
2025-09-11 18:35:07 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,12 @@
+public class Day4 {
+    public static void main(String[] args) {
+    
+    String nama = "Muzita can";
+    int umur = 18;
+    double angkaLahir = 73.2007;
+    
+    System.out.print("Nama \t\t= " + nama + "\nUmur \t\t= " + umur + " tahun");
+    System.out.println("\nAngka Lahir \t= " + angkaLahir );
+    }
+    
+}
