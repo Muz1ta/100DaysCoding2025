7b8fa119d5e66de5fc217dd2603a7dd687b9dac7
Muz1ta <muzitaainnun@gmail.com>
2025-09-12 16:27:15 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+public class Day5 {
+    public static void main(String[] args) {
+    String nama = "Muzitacan";
+    short a = 7;
+    byte b = 3;
+    int c = 27;
+    long d = 73;
+    System.out.println("Nama \t\t= " + nama + "\nTanggal \t= " + a + "\nBulan \t\t= " + b + "\nTahun \t\t= " + c + "\nAngka Fav \t= " + d );
+      
+}
+}
