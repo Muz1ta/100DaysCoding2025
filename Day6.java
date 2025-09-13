1d18f32eb7bbeb9b2bdae9816a8440094ee334b1
Muz1ta <muzitaainnun@gmail.com>
2025-09-13 09:48:33 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class Day6 {
+    public static void main(String[] args) {
+        String nama = "Muzita";
+        float bb = 40.1f;
+        double tinggi = 147.3;
+        System.out.print("Nama \t= " + nama + "\nBb \t= " + bb + "\nTinggi \t= " + tinggi);
+    
+    }
+}
