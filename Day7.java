7f306c180529fa315933351404d9d770b63d426a
Muz1ta <muzitaainnun@gmail.com>
2025-09-14 15:14:26 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,12 @@
+public class Day7 {
+    public static void main (String[] args) {
+        char huruf = 'P';
+        System.out.printf("%c\n", huruf );
+        boolean cantik = true;
+         if (cantik == true) {
+       System.out.printf("Kamu cantik\n");
+         } else { 
+                System.out.printf("Kamu tidak cantik\n"); 
+            }   
+}
+}
