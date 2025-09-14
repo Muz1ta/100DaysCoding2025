cc39199cb0bb654ca0d80449c7c06170cbdfb24f
Muz1ta <muzitaainnun@gmail.com>
2025-09-15 07:29:53 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+public class Day8 {
+    public static void main(String[] args) {
+        String Nama = "Muzita Ainung Reski";
+        String Kenalan = "Nice to meet you, guyss";
+        System.out.println("Nama");
+        System.out.println("Kenalan");
+        
+        /**tipe data string adalah tipe data yang mempresentasikan 
+        * suatu teks seperti nama, alama, dll.
+        */ 
+        
+        String nama = "Muzita Ainung Reski";
+        String kenalan = "Nice to meet you, guyss";
+        System.out.println(nama);
+        System.out.println(kenalan);
+}
+}
