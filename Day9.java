6c00f9e9b3a19fbe87ecb4f16f20e52f50734b46
Muz1ta <muzitaainnun@gmail.com>
2025-09-16 21:47:07 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,9 @@
+public class Day9 {
+    public static void main(String[] args) {
+        int harga= 5;
+        System.out.println("Harga buku awal  = " + harga);
+        harga = 8;
+        System.out.println("Harga buku setelah diupdate =" + harga);
+        
+}
+}
