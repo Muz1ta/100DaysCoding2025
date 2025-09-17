641c45cf5221a3f87f72cbbc87cdcac94c64efb0
Muz1ta <muzitaainnun@gmail.com>
2025-09-17 18:30:02 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+public class Day10 {
+    public static void main(String[] args) {
+        final int MAX_SIZE = 730;
+        // jika memasukkan (update nilai) MAX_SIZE = 800, Maka hasilnya akan eror 
+        System.out.println(MAX_SIZE);
+        
+        
+    }
+    
+}
