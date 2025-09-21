c2a3041e3ee7042334fb1c20d08fb686a574a7fd
Muz1ta <muzitaainnun@gmail.com>
2025-09-21 14:17:42 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,7 @@
+public class Day14 {
+    public static void main(String[] args) {
+        String Nim = "732007";
+        int nim = Integer.parseInt(Nim);
+        System.out.println(nim);
+    } 
+}
