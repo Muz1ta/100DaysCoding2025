bd27d65c7f4624f5b59b375fce764f6ab6636b5d
Muz1ta <muzitaainnun@gmail.com>
2025-09-23 21:06:11 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,18 @@
+public class Day16 {
+    public static void main (String[] args) {
+        System.out.println("Perkalian dan Pembagian");
+        int angka1 = 50;
+        int angka2 = 10;
+        long hasilK = angka1 * angka2;
+        System.out.println("\nNilai Angka1 = " + angka1);
+        System.out.println("Nilai Angka2 = " + angka2);
+        System.out.println("Angka1 × Angka2 = " + hasilK);
+        
+        int A = 100;
+        int B = 10;
+        int hasilB = A / B;
+        System.out.println("\nNilai A = " + A);
+        System.out.println("Nilai B = " + B);
+        System.out.println("A : B = " + hasilB);     
+    }
+}
