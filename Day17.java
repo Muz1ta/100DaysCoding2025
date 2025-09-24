801f62d41d39f6eaecbcfe944cff5c21e16a9d74
Muz1ta <muzitaainnun@gmail.com>
2025-09-24 17:47:00 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class Day17 {
+    public static void main(String[] args) {
+        System.out.println("Modulus/Sisa Bagi");
+        int angka1 = 20;
+        int angka2 = 10;
+        int hasil = angka1 % angka2;
+        System.out.println("\nAngka1 = " + angka1);
+        System.out.println("Angka2 = " + angka2);
+        System.out.println("\nHasil angka1 % angka2 = " + hasil);
+        
+    }
+    
+}
