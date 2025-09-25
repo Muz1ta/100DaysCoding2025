f2eced497ad6de14c09f8eafa1edee5d98efca17
Muz1ta <muzitaainnun@gmail.com>
2025-09-25 17:21:17 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,8 @@
+public class tipeee {
+    public static void main(String[] args){
+        double angkaA = 30.75;
+        System.out.println("Double  = " + angkaA);
+        byte angkaB = (byte) + angkaA;
+        System.out.println("byte = " + angkaB);
+    }
+}
