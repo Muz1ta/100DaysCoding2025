2ec3d2cbcec4575afc2ef6b00ca7facf1c578f29
Muz1ta <muzitaainnun@gmail.com>
2025-09-26 21:50:58 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,8 @@
+public class Day19 {
+    public static void main(String[] args){
+        int a = 732007;
+        System.out.print("Nilai int sebelum konversi \n= "+ a);
+        byte b = (byte)a;
+        System.out.print("\nNilai setelah konversi ke Byte \n= "+b);
+    }
+}
