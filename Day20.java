b273ea8693ce034809e2f758200be060789e9e4c
Muz1ta <muzitaainnun@gmail.com>
2025-09-27 19:03:00 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,21 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int a = 307;
+        String b = Integer.toString(a);
+        System.out.println("Angka lahir = "+a);
+        
+        double c =  73.27;
+        String d = String.format("%f",c);
+        System.out.println("Angka fav   = "+c);
+        
+        char e = 'P';
+        String f = String.valueOf(e);
+        System.out.println("Gender \t    = "+e);
+        
+        boolean g = true;
+        String h = "" + g;
+        System.out.println("Mau tidur   = "+g);
+        
+        
+    }  
+}
