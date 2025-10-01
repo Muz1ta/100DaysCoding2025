ec76d5655f1ed446147eeb81c498a54b9eab6864
Muz1ta <muzitaainnun@gmail.com>
2025-10-02 07:29:06 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,16 @@
+public class indecrement {
+    public static void main(String[] args){
+        int a = 10;
+        a++;
+        System.out.println(a);
+        --a;
+        System.out.println(a);
+        
+        int b = a++;
+        System.out.println("\n"+b);
+        int c = --a;
+        System.out.println(c);
+        
+        
+    }  
+}
