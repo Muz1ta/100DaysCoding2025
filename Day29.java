183344e5c214e2799bdb8a1e94cfe8e2b3c3dd09
Muz1ta <muzitaainnun@gmail.com>
2025-10-06 21:01:37 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,8 @@
+public class Day29 {
+    public static void main(String[] args){
+        int a = 12;
+        int b = 10;
+        System.out.println(a < b);
+        System.out.println(a > b);
+    } 
+}
