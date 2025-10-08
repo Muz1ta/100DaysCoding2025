bf17d221e51834999280ef268c520a5d928f4811
Muz1ta <muzitaainnun@gmail.com>
2025-10-08 22:41:13 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,11 @@
+public class Day31 {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 25;
+        System.out.println(a >= b && a <= b);
+        b = 20;
+        System.out.print(a == b && a >= b);
+        
+                
+    }
+}
