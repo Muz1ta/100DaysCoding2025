6d7d0a080f8f5fa734cbea07b5bd4ea41018113f
Muz1ta <muzitaainnun@gmail.com>
2025-10-10 20:09:44 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,8 @@
+public class Day33 {
+    public static void main(String[] args){
+        boolean Lapar = true;
+        boolean Laparr = !Lapar;
+        System.out.println("Apakah kamu lapar : "+Lapar);
+        System.out.println("Apakah kamu mau Makan : "+Laparr);
+    }
+}
