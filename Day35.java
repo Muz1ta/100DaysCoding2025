f81539e5b89991c8048fafd697144b1f9b50308f
Muz1ta <muzitaainnun@gmail.com>
2025-10-12 21:25:43 +0800

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Day35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+//        program menentukan harga tiket beerdasarkan umur
+         System.out.println("Masukkan umur : ");
+         int umur = sc.nextInt();
+         if (umur < 12){
+             System.out.println("Rp. 25.000");
+         }else if (umur >= 12 && umur < 18){
+             System.out.println("Rp. 35.000");
+         }else{
+             System.out.println("Rp. ");
+         }
+      }   
+    }
