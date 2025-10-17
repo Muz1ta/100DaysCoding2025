71eab1947f8343b97c2a5935b0f98e27eb40bd9f
Muz1ta <muzitaainnun@gmail.com>
2025-10-17 20:45:43 +0800

    Day40.java

--- Day40.java
+++ Day40.java
@@ -9,19 +9,19 @@ public class latihan55 {
         System.out.print("Masukkan angka kedua : ");
         int angka2 = sc.nextInt();
         System.out.print("Pilih operasi (*,/,+,-) : ");
-        String op = sc.next();
+        char op = sc.next().charAt(0);
         
-        if (op.equals("*")){
+        if (op == '*'){
              System.out.print("Hasil "+angka1 + " x " + angka2+ " : "+ (angka1 * angka2));
-        }else if (op.equals("/")){
+        }else if (op == '/'){
           if (angka2 != 0){
             System.out.print("Hasil "+angka1 + " : " + angka2+ " : "+ (angka1 / angka2));
           }else{
             System.out.print("Eror ");
           }
-        }else if (op.equals("+")){
+        }else if(op == '*'){
             System.out.print("Hasil "+angka1 + " + " + angka2+ " : "+ (angka1 + angka2));
-        }else if (op.equals("-")){
+        }else if (op == '*'){
             System.out.print("Hasil "+angka1 + " - " + angka2+ " : "+ (angka1 - angka2));
         }else{
             System.out.print("Tidak Valid");
