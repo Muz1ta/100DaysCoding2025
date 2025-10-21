02d03cbda278dd79d479b1170dbe8de6378956ac
Muz1ta <muzitaainnun@gmail.com>
2025-10-21 10:06:52 +0800

    Day44.java

---
+++ Day44.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day44 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Masukkan nilai : ");
+        int nilai = sc.nextInt();
+        
+        System.out.println("-PREDIKAT NILAI-");
+        
+        if (nilai >= 90 ){
+            System.out.println("Sangat Baik");
+        }else if (nilai >= 80 && nilai < 90){
+            System.out.println("Baik");
+        }else if (nilai >= 70 && nilai < 80){
+            System.out.println("Cukup");
+        }else{
+            System.out.println("Kurang");
+        System.out.println("Tetap Semangat");
+        
+   
+        }
+    }
+    
+}
