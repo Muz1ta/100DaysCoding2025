3ad361d15df478fd093bba29d90429a309f56c82
Muz1ta <muzitaainnun@gmail.com>
2025-10-27 21:34:24 +0800

    Day50.java

---
+++ Day50.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Day50 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan angka : ");
+        int angka = sc.nextInt();
+        
+        String hasil = (angka %2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil"; 
+    System.out.println("Hasil : "+hasil);
+}
+}
