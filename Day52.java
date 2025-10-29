a337bb013402f7eca604139eab4c5e5e832b6db9
Muz1ta <muzitaainnun@gmail.com>
2025-10-29 22:18:15 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+public class Day52 {
+    public static void main(String[] args) {
+       
+        int a = 0;
+        
+        while (a <= 10){
+            a++;
+            System.out.println("Angka "+a);
+        }
+    }
+}
