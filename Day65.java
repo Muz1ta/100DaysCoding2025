8c7caaa8c699cff2603dcb63e86593aa3a99a5a6
Muz1ta <muzitaainnun@gmail.com>
2025-11-11 20:31:59 +0800

    Day65.java

---
+++ Day65.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Day65 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Nilai N : ");
+        int n = sc.nextInt();
+        int f = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+        System.out.println(n+"! : "+f);
+    }
+    
+}
