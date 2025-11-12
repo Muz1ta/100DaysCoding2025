18b21337a87aeeb17a976030e545e535c34680a8
Muz1ta <muzitaainnun@gmail.com>
2025-11-12 22:13:16 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,13 @@
+public class Day66 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 15; i++) {
+        
+        for (int j = 1; j <= 3; j++) {
+            
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+}
