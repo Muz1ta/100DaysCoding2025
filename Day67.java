7e1c1a5715740119c2124851289d1b33f242fb71
Muz1ta <muzitaainnun@gmail.com>
2025-11-13 20:05:08 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,12 @@
+public class Day67 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i < 2; i++) {
+            for (int j = 1; j < 30; j++) {
+                System.out.print("M ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
