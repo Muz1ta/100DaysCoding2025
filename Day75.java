1464c35d1cd51256182d99d39457a3fe6a5bada7
Muz1ta <muzitaainnun@gmail.com>
2025-11-21 22:28:07 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+
+public class Day75 {
+    public static void main(String[] args) {
+       
+        String nama = "Muzita";
+        int jumlah = nama.length();
+        String besar = nama.toUpperCase();
+        String kecil = nama.toLowerCase();
+     
+        System.out.println(nama);
+        System.out.println("===========");
+        System.out.println("Jumlah Huruf : "+jumlah);
+        System.out.println("Ubah ke Huruf besar : "+besar);
+        System.out.println("Ubah ke Huruf Kecil : "+kecil);
+    }
+    
+}
