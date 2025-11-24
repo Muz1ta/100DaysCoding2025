a0456500947dc0e521ecfd0bbe4565c903f8a921
Muz1ta <muzitaainnun@gmail.com>
2025-11-24 23:19:44 +0800

    Day78.java

---
+++ Day78.java
@@ -0,0 +1,12 @@
+public class Day78 {
+    public static void main(String[] args) {
+        
+        String nama = "Muzita Ainung Reski";
+        
+        System.out.println("Panjang nama : "+ nama.length());
+        System.out.println("Nama huruf kecil : "+nama.toLowerCase());
+        System.out.println("Nama dalam huruf besar : "+nama.toUpperCase());
+        System.out.println("Nama (Reski) diganti jadi (cantik) : "+nama.replace("Reski", "Cantik"));
+    }
+    
+}
