e9b49c9404a83452ccb220746940d59387ce28aa
Muz1ta <muzitaainnun@gmail.com>
2025-11-27 22:08:23 +0800

    Day81.java
    Updated the 'nama' array to include an additional name and print it.

--- Day81.java
+++ Day81.java
@@ -1,10 +1,6 @@
 public class Day81 {
     public static void main(String[] args) {
-        String nama [] = {"Muzita", "Ainung", "Reski"};
-        
-        for (int i = 0; i < nama.length; i++) {
-            System.out.print(" "+nama[i]);
+        String nama [] = {"Muzita", "Ainung", "Reski", "Cantik"};
+        System.out.println(nama[3]);
         }
-            
         }
-    }
