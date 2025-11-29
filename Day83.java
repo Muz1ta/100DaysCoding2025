98ca4a736c56a6992d9d78103cd58118c5a0ee42
Muz1ta <muzitaainnun@gmail.com>
2025-11-29 21:42:29 +0800

    Day83.java

---
+++ Day83.java
@@ -0,0 +1,8 @@
+public class Day83 {
+    public static void main(String[] args) {
+        int angka [] = {3,7,37,27};
+        for (int i = 0; i < angka.length; i++) {
+        }
+        System.out.println("Jumlah array : "+angka.length);
+    }
+}
