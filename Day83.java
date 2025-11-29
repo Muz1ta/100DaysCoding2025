e6949d33ccf68fa10632bfd6ebf6305097de83c2
Muz1ta <muzitaainnun@gmail.com>
2025-11-29 22:13:38 +0800

    Day.83.java
    Removed the for loop and added a direct sum of array elements.

--- Day83.java
+++ Day83.java
@@ -1,8 +1,6 @@
 public class Day83 {
     public static void main(String[] args) {
         int angka [] = {3,7,37,27};
-        for (int i = 0; i < angka.length; i++) {
-        }
-        System.out.println("Jumlah array : "+angka.length);
+        System.out.println(angka[0]+angka[1]+angka[2]+angka[3]);
     }
 }
