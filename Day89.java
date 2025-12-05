24f47f6db745f30df5958c6d2831f4025160993b
Muz1ta <muzitaainnun@gmail.com>
2025-12-05 19:07:58 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,11 @@
+public class Day89 {
+    static void luasKebun(int a,int b){
+        int hasil = a * b;
+        System.out.println("Luas Kebun : "+hasil);
+    }
+    public static void main(String[] args) {
+       int a = 5;
+       int b = 9;
+       luasKebun (a , b);
+    }
+}
