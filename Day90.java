3cb26c67c741754fe2c87d5ed9e16ab61bb0c2b0
Muz1ta <muzitaainnun@gmail.com>
2025-12-06 22:24:23 +0800

    Day90.java

---
+++ Day90.java
@@ -0,0 +1,9 @@
+public class Day90 {
+   static int luasKebun (int a, int b){
+       return a * b;
+    }
+    public static void main(String[] args) {
+        int hasil = 0;
+        System.out.println("Luas Kebun : "+luasKebun(5,9));
+    }
+    }
