d4a80cce39fe606513692e2a6aa058e12c9e3e3e
Muz1ta <muzitaainnun@gmail.com>
2025-12-09 18:07:41 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,15 @@
+public class Day93 {
+    public static void main(String[] args) {
+        
+        double muzi = 37.7;
+        
+        double ceil = Math.ceil(muzi);
+        double floor = Math.floor(muzi);
+        double round = Math.round(muzi);
+        
+        System.out.println("Ceil : "+ceil);
+        System.out.println("Floor : "+floor);
+        System.out.println("Round : "+round);
+        
+    }
+}
