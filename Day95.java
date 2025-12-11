4c57ce6c2506d6ee822e3e93126b2613fe143f6b
Muz1ta <muzitaainnun@gmail.com>
2025-12-11 11:23:22 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,9 @@
+public class Day95 {
+    public static void main(String[] args) {
+        cetakPesan("Haii!!!");
+        cetakPesan("Nama Saya Muzita, Nice to meet you guyss!!!");
+    }
+    public static void cetakPesan (String pesan){
+        System.out.println(pesan);
+    }
+}
