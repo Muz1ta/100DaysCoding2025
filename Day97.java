c2ae2354c897c5e9a08cc2a9da007a43a15dfde8
Muz1ta <muzitaainnun@gmail.com>
2025-12-13 21:16:38 +0800

    Day97.java

---
+++ Day97.java
@@ -0,0 +1,8 @@
+public class Day97 {
+    public static void main(String[] args) {
+        System.out.println("Luas Persegi : "+luasPersegi(5));
+    }
+    static int luasPersegi (int sisi){
+       return sisi * sisi;
+    }
+}
